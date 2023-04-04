571311620a4f73a29a20b9326182128271ad6bf9
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-04-04 17:46:33 -0300

    first commit

---
+++ venv/aula11.py
@@ -0,0 +1,10 @@
+# if / elif   / else
+# se / senao se / se nao
+
+entrada = input('Você quer "Entrar" ou "Sair"? ')
+
+if entrada == 'entrada': 
+    print('Você entrou no sistema')
+
+elif entrada == 'sair':
+    print('Você saiu do sistema')
---
+++ venv/aula9.py
@@ -0,0 +1,8 @@
+nome = input('Qual o seu nome? ')
+idade = input('Qual a sua idade? ')
+maior_idade = idade >= 18 
+ano_nascimento = 2003
+
+print(f'o seu nome é {nome}')
+print(f'sua idade é {idade}')
+print(f'maior de idade {maior_idade}')
