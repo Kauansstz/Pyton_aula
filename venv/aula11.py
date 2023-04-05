557dc875c28a49bde71b91989d236450ad156dbe
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-04-05 17:49:38 -0300

    /venv

--- venv/aula11.py
+++ venv/aula11.py
@@ -1,10 +1,13 @@
 # if / elif   / else
-# se / senao se / se nao
+# se / se nao se / se nao 
 
 entrada = input('Você quer "Entrar" ou "Sair"? ')
 
-if entrada == 'entrada': 
+if entrada == 'entrar': 
     print('Você entrou no sistema')
 
 elif entrada == 'sair':
-    print('Você saiu do sistema') 
+   print('Você saiu do sistema') 
+
+else:
+    print('Você não digitou nem entrar e nem sair')
