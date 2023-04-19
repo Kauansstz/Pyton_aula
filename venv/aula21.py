6ceb8a676ce5960b4879cb624f8260a84b8a19dd
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-04-19 14:26:05 -0300

    First commit

---
+++ venv/aula21.py
@@ -0,0 +1,13 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+
+"""
+string = 'luiz Otávio' 
+# outra_variavel = f'{string[:3]}ABC{string[4:}'
+#outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+
+
+print(string.capitalize())
+# capitalize - Primeria letra do primerio nome fica maiusculo
