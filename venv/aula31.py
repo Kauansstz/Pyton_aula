63e3df2f4607cfefad2d4caa3272e531b087fe9a
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-04-28 12:58:28 -0300

    first commit

--- venv/aula31.py
+++ venv/aula31.py
@@ -1,4 +1,10 @@
 """
-Listas em P
+Listas em Python
+Suporta vários valores de qualquer tipo
+Conhecimento reutilizáveis - índice e fatiamento 
+Métodos úteis
+    append, insert, pop, del, clear, extend, +
+Create Read update  Delete
+Criar, ler, alterar, apagar = Lista[i] (Crud)
 
 """
