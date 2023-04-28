8bd707b4d36ec996e1e1de94cf4751effbbb99f4
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-04-28 13:13:53 -0300

    first commit

--- venv/aula31.py
+++ venv/aula31.py
@@ -8,3 +8,16 @@ Create Read update  Delete
 Criar, ler, alterar, apagar = Lista[i] (Crud)
 
 """
+
+lista = [ 10,20,30,40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+
+lista.append(50)
+ultimo_valor1 = lista.pop()
+lista.append(60)
+lista.append(70)
+ultimo_valor2 = lista.pop()
+print(lista, f'Removido: {ultimo_valor1} e {ultimo_valor2}')
