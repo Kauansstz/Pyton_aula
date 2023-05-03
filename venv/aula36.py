d27d3a89cb950a52dcc134777d6ffe52a854bb26
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-05-03 16:39:53 -0300

    first commit

--- venv/aula36.py
+++ venv/aula36.py
@@ -1,6 +1,29 @@
-import  os
+import os
 
 lista = []
 while True:
     print('Selecione uma opção')
-    opcao = input()
+    opcao = input('[i] para inserir [a] para apagar [l] para listar: ')
+
+    if opcao == 'i':
+        os.system('cls')
+        valor = input('Valor:')
+        lista.append(valor)
+    elif opcao == 'a':
+        indice_str = input('Escolha o índice para apagar: ')
+
+        try:
+            indice = int(indice_str)
+            del lista[indice]
+        except:
+            print('Não foi possivel apagar este índice')
+    elif opcao == 'l':
+        os.system('cls')
+
+        if len(lista) == 0:
+            print('Lista vazia')
+
+        for i, valor in enumerate(lista):
+            print(i, valor)
+    else:
+        print('Opção inválida')
