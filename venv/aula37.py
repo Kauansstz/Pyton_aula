a057168ca3519d6f6e1f02a803e2c01fff361994
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-05-04 17:57:02 -0300

    first commit

---
+++ venv/aula37.py
@@ -0,0 +1,14 @@
+'''
+split e join com list e str
+split - divide uma string
+join - une uma string
+'''
+
+frase = 'olha só que, coisa interessante'
+lista_frases = frase.split(',')
+
+
+for i, frase in enumerate(lista_frases):
+    print(lista_frases[i])
+    
+print(lista_frases)
