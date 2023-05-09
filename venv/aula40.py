4df9cb56d8cad03e5e0d4832f2d1f4921c261ed0
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-05-08 21:52:43 -0300

    first commit

---
+++ venv/aula40.py
@@ -0,0 +1,11 @@
+'''
+Operação ternária (condicional de uma linha )
+<valor> if <condicao> else <outro valor>
+'''
+# condicao = 10 == 10
+# variavel = 'Valor' if condicao else 'Outro'
+# print(variavel)
+
+digito = 90
+novo_digito = digito if digito <= 9 else 0
+print(novo_digito)
