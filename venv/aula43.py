087a6ac44e2ad78e26893e1ec88f8c4ff04f3902
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-05-10 21:28:13 -0300

    first commit

---
+++ venv/aula43.py
@@ -0,0 +1,12 @@
+'''
+Arugumentos nomeados e não nomeados em funções Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas argumento(valor)
+'''
+
+def soma(x, y):
+    print(f'{x= } + {y= }', '|', 'x + y =', x + y)
+
+soma(1, 2)
+soma(2, 1)
+soma(4, 3)
