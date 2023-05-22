d2b05d5e07003537838e59f25c665081f6729305
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-05-22 17:54:11 -0300

    first commit

--- venv/exercicio.py
+++ venv/exercicio.py
@@ -2,12 +2,23 @@
 # Crie funções que duplicam, triplicam e quadruplicam
 # o número recebido como parâmetro.
 
+# numero_1 = input('Digite um numero: ')
+# opcao = input('Digite 2x, 3x ou 4x que ira magicamente mudar de numero: ')
+# try:
+#     numero_1_float = float(numero_1)
 
-def dobrar(numero, duplicar):
-    total = 1
-    multiplico = numero * 2
-    for digito in numero, duplicar:
-        total *= digito
-        return multiplico
+# except ValueError:
+#     print('Numero invalido')
+
+# if opcao == '2x' or opcao == '2':
+#     print(f'O numero escolhido foi {numero_1} e duplicou para {numero_1_float * 2}')
+
+# elif opcao == '3x' or opcao == '3':
+#     print(f'O numero escolhido foi {numero_1} e triplicou para {numero_1_float * 3}')
+
+# else:
+#     print(f'O numero escolhido foi {numero_1} e quadruplicou para {numero_1_float * 4}')
     
-print(dobrar(2))
+def duplicar(numero):
+    return numero * 2
+
