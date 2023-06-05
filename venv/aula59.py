2b56d201394db50cff0dd810af6e93349e2f2677
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-06-05 13:34:12 -0300

    first commit

--- venv/aula59.py
+++ venv/aula59.py
@@ -1,4 +1,8 @@
 # Mapeamento de dados em list comprehension
+import pprint
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False, width=40)
 produtos = [
     {'Nome':'p1', 'preco': 20, },
     {'Nome': 'p2', 'preco': 10, },
@@ -9,6 +13,12 @@ novo_produto = [
     {**produto, 'preco':produto['preco']* 1.05 }
     if produto['preco'] > 20 else {**produto}
     for produto in produtos
+    if produto['preco'] > 10
 ]
 
-print(*novo_produto, sep='\n') 
+p(novo_produto)
+# print(*novo_produto, sep='\n') 
+# p(novo_produto)
+
+# lista = [n for n in range(10) if n < 5]
+# print(lista)
